a7c1ad1a32e19cb456e503f692131c5924feb0b2
俞國忠 / Yii Kuo Chong (Brandon) <byu1628@proton.me>
2026-09-21 20:19:24 +0800

    moved main.css into styles and fonts into utilities/fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { geistSans, geistMono } from "@/utilities/fonts";
 
-import "./globals.css";
+import "@/styles/main.css";
 
 export const instant = false;
-
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
     title: "Songbird",
     description: "Food Ordering System for a Digital Malaysia",
 };
 
-type TLayoutProps = Readonly<{children: React.ReactNode}>;
+type TLayoutProps = Readonly<{ children: React.ReactNode }>;
 
-const Layout = ({
-    children,
-}: TLayoutProps) => (
+const Layout = ({ children }: TLayoutProps) => (
     <html
         lang="en"
         className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-        <body className="min-h-full flex flex-col">
-            {children}
-        </body>
+        <body className="min-h-full flex flex-col">{children}</body>
     </html>
-)
+);
 
 export default Layout;
